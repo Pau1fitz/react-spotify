ca9ef88d696b1abdfbb8ab5c69d374deba12830d
Paul Fitzgerald <paulfitzgerald@pauls-air.home>
2017-11-21 17:46:37 +0000

    add player controls

--- src/components/SongPlayer/component.js
+++ src/components/SongPlayer/component.js
@@ -7,6 +7,26 @@ class SongPlayer extends Component {
     return (
       <div className='song-player-container'>
 
+        <div className='song-controls'>
+
+          <div className='reverse-song'>
+            <i class="fa fa-step-backward reverse" aria-hidden="true"></i>
+          </div>
+
+          <div className='play-btn'>
+            <i className="fa fa-play-circle-o play-btn" aria-hidden="true"></i>
+          </div>
+
+          <div className='next-song'>
+            <i class="fa fa-step-forward forward" aria-hidden="true"></i>
+          </div>
+
+        </div>
+
+        <div className='song-progress-container'>
+          <div className='song-progress'></div>
+        </div>
+
       </div>
     );
   }
