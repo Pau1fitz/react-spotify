e84543fc58665aa997593ed3eed55f409acdb616
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-04-23 16:14:05 +0530

    remove album icon

--- src/components/ArtWork/component.js
+++ src/components/ArtWork/component.js
@@ -3,8 +3,17 @@ import PropTypes from 'prop-types';
 import './ArtWork.css';
 
 const ArtWork = (albumArtwork) => (
-  <div className='album-artwork-container'>
-    <img alt="artwork" className='album-artwork' src={albumArtwork.albumImage} />
+  <div className="album-artwork-container">
+    {albumArtwork.albumImage ? (
+      <img
+        alt="artwork"
+        className="album-artwork"
+        src={albumArtwork.albumImage}
+      />
+    ) : (
+      //basically display nothing
+      <h1></h1>
+    )}
   </div>
 );
 
